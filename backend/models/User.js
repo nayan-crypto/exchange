1bc1a6c22d2fcf79f4aa8cf23d9e610a530a28fb
nayan-crypto <njbarman47@gmail.com>
2025-04-30 02:49:44 +0530

    Create User.js

---
+++ backend/models/User.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        enum: ['user', 'admin'],
+        default: 'user'
+    },
+    twoFactorSecret: {
+        type: String // 2FA
+    },
+    twoFactorEnabled: {
+        type: Boolean,
+        default: false
+    },
+    kycVerified: {
+        type: Boolean,
+        default: false
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+});
+
+userSchema.methods.comparePassword = async function (password) {
+    return await bcrypt.compare(password, this.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
